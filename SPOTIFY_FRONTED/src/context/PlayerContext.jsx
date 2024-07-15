23f5ecae4772a1859dacb6bff7621d9b9f7fd991
prasanna00019 <159623027+prasanna00019@users.noreply.github.com>
2024-07-15 15:45:25 +0530

    Update PlayerContext.jsx

--- SPOTIFY_FRONTED/src/context/PlayerContext.jsx
+++ SPOTIFY_FRONTED/src/context/PlayerContext.jsx
@@ -6,7 +6,7 @@ const PlayerContextProvider = (props) => {
   const seekbg = useRef();
   const audioseek=useRef();
   const seekbar = useRef();
-  const  url='https://spotify-backend-ilpx.onrender.com';
+  const  url='https://spotify-frontend-ldf0.onrender.com';
   const [songsData,setsongsdata]=useState([]);
   const[albumsData,setalbumsdata]=useState([]);
   const [track, settrack] = useState(songsData[0]);
